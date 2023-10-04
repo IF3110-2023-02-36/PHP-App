c4fb731fb99c99d3468159fe64f8085e58cf5378
DewanaGustavus <76590469+DewanaGustavus@users.noreply.github.com>
2023-10-04 13:07:21 +0700

    feat: login styling

--- src/App/components/login/Register.php
+++ src/App/components/login/Register.php
@@ -9,6 +9,7 @@ if (isset($_SESSION["user_username"])) {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../../public/styles/login/Login.css">
     <title>register</title>
 </head>
 
