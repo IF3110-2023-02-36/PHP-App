c31e8bd84edf47f646379c771abbf3577348695d
DewanaGustavus <76590469+DewanaGustavus@users.noreply.github.com>
2023-10-05 23:37:02 +0700

    refactor: automatic routing

--- src/App/views/Routes.php
+++ src/App/views/Routes.php
@@ -1,31 +1,42 @@
 <?php
 
+// automatic routing
+$componentDir = __DIR__ . '/../components';
+$folders = scandir($componentDir);
+unset($folders[0]);
+unset($folders[1]);
+$routes = array();
+foreach ($folders as $folderName) {
+    $folderDir = $componentDir . "/$folderName";
+    $files = scandir($folderDir);
+    unset($files[0]);
+    unset($files[1]);
+    foreach ($files as $file) {
+        $fileName = str_replace('.php', '', $file);
+        $routes[$fileName] = $folderName;
+    }
+}
+
+// manual routing
 // routes mapping filename -> folder
-$routes = array(
-    'AddCart' => "cart",
-    'Cart' => "cart",
-    'Home' => "home",
-    'NotFound' => "home",
-    'Login' => "login",
-    'Logout' => "login",
-    'Register' => "login",
-    'Manage' => "manage",
-    'ManageProduct' => "manage",
-    'AddProduct' => "product",
-    'Product' => "product",
-    'EditProduct' => "product",
-    'Category' => "category",
-    'AddCategory' => "category",
-    'EditCategory' => "category",
-    'DeleteCategory' => "category",
-    'Profile' => "profile",
-    'Search' => "search",
-);
-
-
-
-
-
-
-
+// $routes = array(
+//     'AddCart' => "cart",
+//     'Cart' => "cart",
+//     'Home' => "home",
+//     'NotFound' => "home",
+//     'Login' => "login",
+//     'Logout' => "login",
+//     'Register' => "login",
+//     'Manage' => "manage",
+//     'ManageProduct' => "manage",
+//     'AddProduct' => "product",
+//     'Product' => "product",
+//     'EditProduct' => "product",
+//     'Category' => "category",
+//     'AddCategory' => "category",
+//     'EditCategory' => "category",
+//     'DeleteCategory' => "category",
+//     'Profile' => "profile",
+//     'Search' => "search",
+// );
 
