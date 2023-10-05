d07bc431a2897a261beab471a0d1fb91b9193082
DewanaGustavus <76590469+DewanaGustavus@users.noreply.github.com>
2023-10-05 23:55:35 +0700

    fix: automatic routing using controller

--- src/App/views/Routes.php
+++ src/App/views/Routes.php
@@ -1,18 +1,19 @@
 <?php
 
 // automatic routing
-$componentDir = __DIR__ . '/../components';
+$componentDir = __DIR__ . '/../controller';
 $folders = scandir($componentDir);
 unset($folders[0]);
 unset($folders[1]);
 $routes = array();
 foreach ($folders as $folderName) {
     $folderDir = $componentDir . "/$folderName";
+    if($folderName === "Controller.php") continue;
     $files = scandir($folderDir);
     unset($files[0]);
     unset($files[1]);
     foreach ($files as $file) {
-        $fileName = str_replace('.php', '', $file);
+        $fileName = str_replace('Controller.php', '', $file);
         $routes[$fileName] = $folderName;
     }
 }
