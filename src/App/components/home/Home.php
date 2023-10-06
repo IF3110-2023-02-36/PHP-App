04befd03deceab8c85eb8b85e98731cb43bd6281
DewanaGustavus <76590469+DewanaGustavus@users.noreply.github.com>
2023-10-06 13:42:11 +0700

    feat: search keep query param

--- src/App/components/home/Home.php
+++ src/App/components/home/Home.php
@@ -15,19 +15,30 @@
     include(dirname(__DIR__) . '/template/Navbar.php');
     ?>
 
-    <form action="/Search" method="POST">
+    <form action="" method="POST">
         <label for="search">Search</label>
-        <input type="text" name="search" placeholder="Search">
+        <input type="text" name="search" placeholder="Search" 
+                value="<?= $this->data['search'] ?>">
 
         <label for="sort">Sort by</label>
         <select name="sort" id="sort">
-            <option value="name">nama</option>
-            <option value="price">harga</option>
-            <option value="stock">stok</option>
+            <option value="name" 
+                <?php if($this->data['sort'] == "name")echo "selected";?>>
+                nama</option>
+            <option value="price" 
+                <?php if($this->data['sort'] == "price")echo "selected";?>>
+                harga</option>
+            <option value="stock" 
+                <?php if($this->data['sort'] == "stock")echo "selected";?>>
+                stok</option>
         </select>
         <select name="order" id="order">
-            <option value="ASC">ASC</option>
-            <option value="DESC">DESC</option>
+            <option value="ASC"
+                <?php if($this->data['order'] == "ASC")echo "selected";?>>>
+                ASC</option>
+            <option value="DESC"
+                <?php if($this->data['order'] == "DESC")echo "selected";?>>>
+                DESC</option>
         </select>
 
         <label for="category">Category</label>
@@ -35,27 +46,27 @@
             <option value=''></option>
             <?php 
                 foreach($this->data['category'] as $category){
+                    $isSelected = ($this->data['category_id'] == $category[0]);
+                    $selected = $isSelected ? 'selected' : '';
                     echo "
-                        <option value='$category[0]'>$category[1]</option>
+                        <option value='$category[0]' $selected>$category[1]</option>
                     ";
                 }
             ?>
         </select>
 
         <label for="min-price">Min Price:</label>
-        <input type="number" name="min-price">
+        <input type="number" name="min-price" value=<?= $this->data['min-price']?>>
     
         <label for="max-price">Max Price:</label>
-        <input type="number" name="max-price">
+        <input type="number" name="max-price" value=<?= $this->data['max-price']?>>
 
         <button type="submit">search</button>
     </form>
 
     <span class="background">
         <span class="centering">
-            <h><?php if(isset($_SESSION['user_name']))echo "Halo " . $_SESSION['user_name'];?></h>
-            <h1>Home Page</h1>
-            <h2>List barang mu</h2>
+            <h1>Search</h1>
             <section class="articles">
                 <?php
                 include_once(dirname(__DIR__) . "/template/ProductCard.php");
@@ -69,7 +80,7 @@
                                 $product[2],
                                 $product[4],
                                 $product[3],
-                                "AIPONG",
+                                $product[2],
                                 $product[0]);
                             break;
                         }
